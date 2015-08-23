935fb9f2a5a182325b10778ee16e74ec8cd768c4
GuillaumeUnice <guillaume.unice@gmail.com>
2015-08-23 20:27:43 +0200

    Suppression css inutile + refactoring de image management (pensez a sortir de save le crop and resize) + prevoir pour utilisateur upload aps pour maintenant

--- app/Management/ImageManagementInterface.php
+++ app/Management/ImageManagementInterface.php
@@ -9,13 +9,23 @@ interface ImageManagementInterface {
      * permettant d'uploader une image dans le compte utilisateur
      * Ou app si utilisateur de type admin
      * @param $image image
-     * @return mixed
+     * @return boolean
      */
     public function save($image);
+
+    /**
+     * permettant de supprimer une image
+     * @param $pathFilename string
+     * @return boolean
+     */
+    public function delete($pathFilename);
 
     public function getErrorMessage();
 
     public function getSuccessMessage();
+
+    public function getfileName();
+
 
 
 }
--- app/Management/Impl/ImageManagement.php
+++ app/Management/Impl/ImageManagement.php
@@ -4,103 +4,126 @@ namespace App\Management\Impl;
 
 
 use App\Management\ImageManagementInterface;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\Facades\Image;
 
 class ImageManagement implements ImageManagementInterface {
-    protected $user;
-    private $errorMessage = '';
-    private $successMessage = '';
+  protected $user;
+  private $errorMessage = '';
+  private $successMessage = '';
+  private $fileName = '';
 
-    public function getErrorMessage() {
-        return $this->errorMessage;
-    }
-    public function getSuccessMessage() {
-        return $this->successMessage;
-    }
+  public function getErrorMessage() {
+    return $this->errorMessage;
+  }
+  public function getSuccessMessage() {
+    return $this->successMessage;
+  }
 
-    function __construct() {
-        $this->user = Auth::user();
-    }
+  public function getfileName() {
+    return $this->fileName;
+  }
 
-    /**
-     * permettant d'uploader une image dans le compte utilisateur
-     * Ou app si utilisateur de type admin
-     * @param $image image
-     * @return bool
-     */
-    public function save($image) {
-        $destinationPath = $this->getDestinationPath($image->getSize());
-        if($destinationPath != false) {
-            $fileName = $this->getFileNameAvailable($destinationPath, $image);
+  function __construct() {
+    $this->user = Auth::user();
+  }
 
-            if(Auth::user()->role >= config('role.administrator')) {
-                $image->move($destinationPath, $fileName); // uploading file to given path
-                $this->successMessage = $destinationPath . '/' . $fileName;
-                return true;
-            } else {
-                DB::transaction(function() use ($destinationPath, $fileName, $image)
-                {
-                    $this->user->image_size += $image->getSize();
-                    $this->user->save();
-                    $image->move($destinationPath, $fileName); // uploading file to given path
-                });
-                $this->successMessage = $destinationPath . '/' . $fileName;
-                return true;
-            }
-
-        } else {
-            $this->errorMessage = 'L\'upload n\'est pas possible votre espace autorisee est plein!';
-            return false;
-        }
-
+  /**
+   * permettant d'uploader une image dans le compte utilisateur
+   * Ou app si utilisateur de type admin
+   * @param $image image
+   * @return bool
+   */
+  public function save($image, $destinationPath = null, $crop = null, $resize = null) {
+    if($destinationPath == null){
+      $destinationPath = $this->getDestinationPath($image->getSize());
     }
 
-    /**
-     * Cette fonction permet de renommer le nom d'une image
-     * si cette dernière possède un nom déjà existant.
-     * Le rennomage s'effectue en prefixant le nom pas un chiffre.
-     * @param $destinationPath repertoire final de destination de l'image uploadé
-     *
-     * @return mixed
-     */
-    private function getFileNameAvailable($destinationPath, $file) {
+    if($destinationPath != false) {
+      $fileName = $this->getFileNameAvailable($destinationPath, $image);
+      $interventionImage = Image::make( $image->getRealPath());
+      if($resize) {
+        $interventionImage->resize($resize['width'], $resize['height']);
+      }
+      if($crop) {
+        $interventionImage->crop($crop['width'], $crop['height']);
+      }
 
-        $fileName =  $file->getClientOriginalName(); // full name + extension
+      //enregistrement nouvelle taille autorise a utilisateur
+      if(!Auth::user()->role >= config('role.administrator')) {
+        $this->user->image_size += $image->getSize();
+        $this->user->save();
+      }
 
-        if(file_exists($destinationPath . '/' . $fileName)) {
-            $i = 1;
-            $fileName = substr_replace($fileName, (string)$i, 0, 0);
-            while(file_exists($destinationPath . '/' . $fileName)) {
-                $fileName = substr_replace($fileName, (string)$i, 0, 1);
-                $i++;
-            }
-        }
-        return $fileName;
+      // enregistrement image ficher tmp -> destination
+      $interventionImage->save($destinationPath . $fileName)->destroy();
+
+      // param retour
+      $this->fileName = $fileName;
+      $this->successMessage = $destinationPath . '/' . $fileName;
+      return true;
+    } else {
+      $this->errorMessage = 'L\'upload n\'est pas possible votre espace autorisee est plein!';
+      return false;
     }
-    /*
-     * Cette fonction evalue s'il reste assez d'espace autorisee par l'application
-     * pour permettre a l'utilisateur d'uploader son image
-     * @param $user User utilisateur concernee
-     * @param $sizeFile taille en Ko de l'image devant être uploadee
-     *
-     * @return boolean true si l'utilisateur dispose de l'espace requis
-     */
-    private function isSpaceAvailable($user, $sizeFile) {
-        return (($user->image_size + $sizeFile) <= config('upload.maxSizeByUser'));
+  }
+
+  /**
+   * Cette fonction permet de renommer le nom d'une image
+   * si cette dernière possède un nom déjà existant.
+   * Le rennomage s'effectue en prefixant le nom pas un chiffre.
+   * @param $destinationPath repertoire final de destination de l'image uploadé
+   *
+   * @return mixed
+   */
+  private function getFileNameAvailable($destinationPath, $file) {
+
+    $fileName =  $file->getClientOriginalName(); // full name + extension
+
+    if(file_exists($destinationPath . '/' . $fileName)) {
+      $i = 1;
+      $fileName = substr_replace($fileName, (string)$i, 0, 0);
+      while(file_exists($destinationPath . '/' . $fileName)) {
+        $fileName = substr_replace($fileName, (string)$i, 0, 1);
+        $i++;
+      }
     }
+    return $fileName;
+  }
+  /*
+   * Cette fonction evalue s'il reste assez d'espace autorisee par l'application
+   * pour permettre a l'utilisateur d'uploader son image
+   * @param $user User utilisateur concernee
+   * @param $sizeFile taille en Ko de l'image devant être uploadee
+   *
+   * @return boolean true si l'utilisateur dispose de l'espace requis
+   */
+  private function isSpaceAvailable($user, $sizeFile) {
+    return (($user->image_size + $sizeFile) <= config('upload.maxSizeByUser'));
+  }
 
-    private function getDestinationPath($sizeFile) {
+  private function getDestinationPath($sizeFile) {
 
 
-        if(Auth::user()->role >= config('role.administrator')) {
-            $destinationPath = config('upload.pathApp') . config('upload.pathImages');
-        } else {
+    if(Auth::user()->role >= config('role.administrator')) {
+      $destinationPath = config('upload.pathApp') . config('upload.pathImages');
+    } else {
 
-            $destinationPath = config('upload.pathUser') . $this->user->username . '/' . config('upload.pathImages');
-            if(!$this->isSpaceAvailable($this->user, $sizeFile))
-                return false;
-        }
-        return $destinationPath;
+      $destinationPath = config('upload.pathUser') . $this->user->username . '/' . config('upload.pathImages');
+      if(!$this->isSpaceAvailable($this->user, $sizeFile))
+        return false;
     }
+    return $destinationPath;
+  }
+
+  /**
+   * permettant de supprimer une image
+   * @param $pathFilename string
+   * @return boolean
+   */
+  public function delete($pathFilename) {
+    File::delete($pathFilename);
+    return File::exists($pathFilename);
+  }
 }
